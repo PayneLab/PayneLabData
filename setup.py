e8d0af0ad186cecbabaf2b943a93ea0652d8dec1
seanjib <seanjib@gmail.com>
2019-01-24 11:46:37 -0700

    initial commit. CPTAC is contained, docs folder should probably be migrated to the individual project. Beginning the FabricatedData package but nothing that is currently in there should be taken seriously.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+version = {}
+with open('PayneLabData/version.py') as fp:
+	exec(fp.read(), version)
+
+setup(name='PayneLabData',
+	version=version['__version__'],
+	description='Data for Professor Samuel Payne\'s research lab at Brigham Young University',
+	url='http://github.com/PayneLab/PayneLabData',
+	author='Dr. Samuel Payne',
+	author_email='sam_payne@byu.edu',
+	license='Apache 2.0',
+	packages=[
+		'PayneLabData',
+		'PayneLabData.CPTAC'
+		],
+	#packages=find_packages(),
+	install_requires=[
+		'numpy',
+		'pandas'
+	],
+	include_package_data=True
+	)
